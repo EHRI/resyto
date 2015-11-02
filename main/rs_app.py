f8133456c25952f66ac1d181aa6d5c2c9ea9ae28
henk van den berg <ecco@Hor-Vastus.local>
2015-11-02 09:38:37 +0100

    .gitignore punch

---
+++ main/rs_app.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtCore import QCoreApplication
+
+class RsApplication(QCoreApplication):
+
+    def __init__(self, args):
+        super.__init__(self)
