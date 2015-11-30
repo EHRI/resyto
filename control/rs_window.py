9f7e2da3b56253d3b7cbe14fbdcac2056b2a2e96
henk van den berg <ecco@hor-vastus.ia.knaw.local>
2015-11-30 14:35:13 +0100

    menu choice syncs with tab choice

--- control/rs_window.py
+++ control/rs_window.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QTabWidget, QWidget
+from PyQt5.QtWidgets import QMainWindow, QAction, QActionGroup, qApp, QTabWidget, QWidget
 
 from view.config_frame import ConfigFrame
 from view.upload_frame import UploadFrame
@@ -17,11 +17,14 @@ class RsMainWindow(QMainWindow):
         self.resize(780, 380)
         self.show()
 
-    def show_configure(self):
-        self.tabframe.setCurrentIndex(0)
+    def on_tab_choice(self, tabnr):
+        self.tabframe.setCurrentIndex(tabnr)
+        self.set_tab_menu_enabled(tabnr)
 
-    def show_upload(self):
-        self.tabframe.setCurrentIndex(1)
+    def set_tab_menu_enabled(self, tabnr):
+        for action in self.tab_actions:
+            action.setEnabled(True)
+        self.tab_actions[tabnr].setEnabled(False)
 
     def create_menu(self):
 
@@ -33,44 +36,63 @@ class RsMainWindow(QMainWindow):
         show_configure_action = QAction("&Configure", self)
         show_configure_action.setShortcut("Ctrl+C")
         show_configure_action.setStatusTip("Configure rsync")
-        show_configure_action.triggered.connect(self.show_configure)
+        show_configure_action.triggered.connect(lambda: self.on_tab_choice(0))
 
         show_upload_action = QAction("&Upload", self)
         show_upload_action.setShortcut("Ctrl+U")
-        show_upload_action.setStatusTip("Uploafd files")
-        show_upload_action.triggered.connect(self.show_upload)
+        show_upload_action.setStatusTip("Upload files")
+        show_upload_action.triggered.connect(lambda: self.on_tab_choice(1))
 
+        show_statistics_action = QAction("&Statistics", self)
+        show_statistics_action.setShortcut("Ctrl+S")
+        show_statistics_action.setStatusTip("Statistics")
+        show_statistics_action.triggered.connect(lambda: self.on_tab_choice(2))
 
-        some_action = QAction("&Something", self)
+        show_man_sets_action = QAction("&Manual Sets", self)
+        show_man_sets_action.setShortcut("Ctrl+M")
+        show_man_sets_action.setStatusTip("Manual Sets")
+        show_man_sets_action.triggered.connect(lambda: self.on_tab_choice(3))
 
-        showMainAction = QAction("&Main window", self)
+        show_rule_based_sets_action = QAction("&Rule-based Sets", self)
+        show_rule_based_sets_action.setShortcut("Ctrl+R")
+        show_rule_based_sets_action.setStatusTip("Rule-based Sets")
+        show_rule_based_sets_action.triggered.connect(lambda: self.on_tab_choice(4))
 
         self.menubar = self.menuBar()
         self.menubar.setNativeMenuBar(True)
 
-        fileMenu = self.menubar.addMenu("&File")
-        fileMenu.addAction(some_action)
-        fileMenu.addAction(exit_action)             # on mac under [application] > Quit [application]
+        self.fileMenu = self.menubar.addMenu("&File")
+        self.fileMenu.addAction(exit_action)             # on mac under [application] > Quit [application]
 
-        viewMenu = self.menubar.addMenu("&View")
-        viewMenu.addAction(showMainAction)
-        viewMenu.addAction(show_configure_action)   # on mac under [application] > Preverences
-        viewMenu.addAction(show_upload_action)
+        self.viewMenu = self.menubar.addMenu("&View")
+        self.viewMenu.addAction(show_configure_action)   # on mac under [application] > Preverences
+        self.viewMenu.addAction(show_upload_action)
+        self.viewMenu.addAction(show_statistics_action)
+        self.viewMenu.addAction(show_man_sets_action)
+        self.viewMenu.addAction(show_rule_based_sets_action)
+
+        self.tab_actions = list()
+        self.tab_actions.append(show_configure_action)      # 0
+        self.tab_actions.append(show_upload_action)         # 1
+        self.tab_actions.append(show_statistics_action)     # 2
+        self.tab_actions.append(show_man_sets_action)       # 3
+        self.tab_actions.append(show_rule_based_sets_action) # 4
 
 
 class TabbedFrame(QTabWidget):
 
     def __init__(self, parent):
         super().__init__(parent)
+        self.parent = parent
         self.currentChanged.connect(self.__tabchanged)
         self.previndex = -1
         self.initUI()
 
     def __tabchanged(self, index):
-        print(index)
         if self.previndex > -1:
             self.widget(self.previndex).hide()
         self.previndex = index
+        self.parent.set_tab_menu_enabled(index)
 
 
     def initUI(self):
